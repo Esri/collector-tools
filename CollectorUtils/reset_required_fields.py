8c824dfde2c4d90c2a285a9da91e9bfea6ece0fe
Nitin <nbandi@esri.com>
2017-04-17 09:58:04 -0700

    Code review changes

--- CollectorUtils/reset_required_fields.py
+++ CollectorUtils/reset_required_fields.py
@@ -1,3 +1,17 @@
+"""
+   Copyright 2017 Esri
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.​
+    This sample copies assignments from one project to another feature service
+"""
+    
 import arcpy
 import argparse
 import json
@@ -98,7 +112,7 @@ def update_required_fields(item_id, org_url, token):
     return errors
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Configure GNSS Popup Info")
+    parser = argparse.ArgumentParser("Reset Required fields to null")
     parser.add_argument('item_ids', help="The id of the map to use", nargs="+")
     # optional args for commandline use
     parser.add_argument('-u',dest ='username', help="The username to authenticate with")
