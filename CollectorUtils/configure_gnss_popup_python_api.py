2b5d5da67b595a82fafcbec941b56c2bbfcfa53d
Nitin <nbandi@esri.com>
2017-12-20 16:13:48 -0800

    Updates to Configure GNSS fields in popup

--- CollectorUtils/configure_gnss_popup_python_api.py
+++ CollectorUtils/configure_gnss_popup_python_api.py
@@ -14,9 +14,44 @@ import arcpy
 import argparse
 import sys
 import json
+import urllib.request
+import urllib.parse
 import arcgis
 from arcgis.gis import GIS
 
+# Retrieve feature service item data.
+def get_data(itemId, params):
+    featureServiceItem_Url = "{}/sharing/rest/content/items/{}/data".format(args_parser.url, itemId)
+    updateResponse = urllib.request.urlopen(featureServiceItem_Url,bytes(urllib.parse.urlencode(params),'utf-8')).read()
+    return json.loads(updateResponse.decode("utf-8"))
+
+# Retrieve feature service item owner and folder Id
+def get_details(itemId, params):
+    featureServiceItem_Url = "{}/sharing/rest/content/items/{}".format(args_parser.url, itemId)
+    updateResponse = urllib.request.urlopen(featureServiceItem_Url,bytes(urllib.parse.urlencode(params),'utf-8')).read()
+    return json.loads(updateResponse.decode("utf-8"))
+
+# Update feature service item (Popup info)
+def update_feature_service(itemId,owner,folder_id,gis,feaureService_data):
+    params = {
+        "token": gis._con.token,
+        "text": json.dumps(feaureService_data).encode('utf8', errors='replace'),
+        "f": "json"
+        }
+
+    if folder_id and folder_id != "":
+        featureServiceItem_Url = "{}/sharing/rest/content/users/{}/{}/items/{}/update".format(args_parser.url,owner,folder_id, itemId)
+    else:
+        featureServiceItem_Url = "{}/sharing/rest/content/users/{}/items/{}/update".format(args_parser.url,owner, itemId)
+    
+    updateResponse = urllib.request.urlopen(featureServiceItem_Url,bytes(urllib.parse.urlencode(params),'utf-8')).read()
+    result = json.loads(updateResponse.decode("utf-8"))
+    if 'error' in result.keys():
+        ex = result['error']['message']
+        arcpy.AddMessage("Error..{}".format(ex))
+    else:
+        arcpy.AddMessage("Siccessfully configured popup and visibility on the Feature service Item..")
+    
 
 # Parse Command-line arguments
 def parseArguments():
@@ -50,76 +85,136 @@ def searchItems_UpdateGNSSMetadataFieldsPopup(args_parser):
         # Iterate through each ItemId and update the popup info for the specified feature layer
         webmapItem = gis.content.search(itemId, item_type="Web Map")  # create a Webmap object from the search result
         webmap = arcgis.mapping.WebMap(webmapItem[0])
+
+        if 'popupInfo' in webmap['operationalLayers'][args_parser.layerIndex].keys():
+            arcpy.arcpy.AddMessage("Service from ArcMap..")
+            # Configure popup and set visibility on the GNSSMetadata fields.
+            fieldInfos = webmap['operationalLayers'][args_parser.layerIndex]['popupInfo']['fieldInfos'] if args_parser.layerIndex else \
+                         webmap['operationalLayers'][0]['popupInfo']['fieldInfos']
+        else:
+            arcpy.arcpy.AddMessage("Service from Pro..")
+            featureServiceItemId = webmap['operationalLayers'][args_parser.layerIndex]['itemId'] if args_parser.layerIndex else \
+                                   webmap['operationalLayers'][0]['itemId']
+            featureServiceItem = gis.content.search(featureServiceItemId, item_type="Feature Service")
+            params = {
+                "token": gis._con.token,
+                "f": "json"
+                }
         
-        # Configure popup and set visibility on the GNSSMetadata fields.
-        fieldInfos = webmap['operationalLayers'][args_parser.layerIndex]['popupInfo']['fieldInfos'] if args_parser.layerIndex else \
-                     webmap['operationalLayers'][0]['popupInfo']['fieldInfos']
+            feature_service_data = get_data(featureServiceItemId,params)
+            feature_service_details = get_details(featureServiceItemId,params)
+
+            owner = feature_service_details['owner']
+            folder_Id = feature_service_details['ownerFolder']
+            
+            fieldInfos = feature_service_data['layers'][args_parser.layerIndex]['popupInfo']['fieldInfos'] if args_parser.layerIndex else \
+                         feature_service_data['layers'][0]['popupInfo']['fieldInfos']            
+            
 
         for field_info in fieldInfos:
             # Configure popup and visibility for GNSSMetadata fields
             if field_info['fieldName'].upper() == 'ESRIGNSS_H_RMS':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2}                    
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_V_RMS':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2}                
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_LATITUDE':
-                field_info['format']['places'] = 8
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 8
+                else:
+                    field_info['format'] = {'places' : 8}                
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_LONGITUDE':
-                field_info['format']['places'] = 8
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 8
+                else:
+                    field_info['format'] = {'places' : 8}                 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_ALTITUDE':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2}                 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_PDOP':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2} 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_HDOP':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2} 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_VDOP':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2} 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_CORRECTIONAGE':
-                field_info['format']['places'] = 0
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 0
+                else:
+                    field_info['format'] = {'places' : 0} 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_FIXDATETIME':
-                field_info['format']['dateFormat'] = 'shortDateShortTime'
+                if 'format' in field_info.keys():
+                    field_info['format']['dateFormat'] = 'shortDateShortTime'
+                else:
+                    field_info['format'] = {'dateFormat' : 'shortDateShortTime'}                    
                 field_info['format']['timezone'] = 'utc'
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_AVG_H_RMS':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2} 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_AVG_V_RMS':
-                field_info['format']['places'] = 2
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 2
+                else:
+                    field_info['format'] = {'places' : 2} 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
             if field_info['fieldName'].upper() == 'ESRIGNSS_H_STDDEV':
-                field_info['format']['places'] = 3
+                if 'format' in field_info.keys():
+                    field_info['format']['places'] = 3
+                else:
+                    field_info['format'] = {'places' : 3} 
                 field_info['visible'] = True
                 field_info['isEditable'] = False
 
@@ -133,13 +228,24 @@ def searchItems_UpdateGNSSMetadataFieldsPopup(args_parser):
 
         # Set Webmap fieldInfos property
         if args_parser.layerIndex:
-            webmap['operationalLayers'][args_parser.layerIndex]['popupInfo']['fieldInfos'] = fieldInfos
+            if 'popupInfo' in webmap['operationalLayers'][args_parser.layerIndex].keys():
+                webmap['operationalLayers'][args_parser.layerIndex]['popupInfo']['fieldInfos'] = fieldInfos
+            else:
+                #webmap['operationalLayers'][args_parser.layerIndex]['popupInfo'] = {'fieldInfos' : fieldInfos}
+                #feature_service_data['layers'][args_parser.layerIndex]['popupInfo']['fieldInfos'] = fieldInfos
+                #update_feature_service(featureServiceItemId, owner, folder_Id, gis,feature_service_data)                
+                
         else:
-            webmap['operationalLayers'][0]['popupInfo']['fieldInfos'] = fieldInfos
+            if 'popupInfo' in webmap['operationalLayers'][0].keys():
+                webmap['operationalLayers'][0]['popupInfo']['fieldInfos'] = fieldInfos
+            else:
+                #webmap['operationalLayers'][0]['popupInfo'] = {'fieldInfos' : fieldInfos}
+                #feature_service_data['layers'][0]['popupInfo']['fieldInfos'] = fieldInfos
+                #update_feature_service(featureServiceItemId, owner, folder_Id,gis, feature_service_data)
 
         # Update Webmap
         webmap.update()
-        arcpy.AddMessage("Siccessfully configured popup and visibility..")
+        arcpy.AddMessage("Siccessfully configured popup and visibility on the Webmap..")
     
     
     except Exception as e:
